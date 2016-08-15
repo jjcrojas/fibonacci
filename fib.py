a5241e83b063ed1680debb37deeb53ae5a441ae1
jjcrojas <jjuancarlosrojas@gmail.com>
2016-08-14 22:39:11 -0500

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,3 +1,4 @@
+#http://www.programiz.com/python-programming/examples/fibonacci-recursion
 def recur_fibo(n):
    """Funcion Recursiva"""
    if n <= 1:
@@ -6,12 +7,13 @@ def recur_fibo(n):
        return(recur_fibo(n-1) + recur_fibo(n-2))
 
 
-# Numero dado por usuario
-nterms = int(input("How many terms? "))
+# Se toma el valor ingresado porel usuario
+nterms = int(input("Ingrese el numero al que se le calculara la serie? "))
 
-# Mirar si el numero es valido
+# Se mira si el numero dado es valido
+#Prueba de comentario
 if nterms <= 0:
-   print("Por favor ingrese un enero positivo")
+   print("Por favor ingresar un entero positivo")
 else:
    print("Serie Fibonacci:")
    for i in range(nterms):
